3036b162113e51a2137ef8261060b3190a4c95e9
Johannes Lyda <jlyda@bmat.com>
2016-10-21 20:07:38 +0200

    Now interactive

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 import yaml
 import magic
 import logging
+import time
 
 config_file = open('config.yaml', 'r')
 config_yaml = yaml.load(config_file)
@@ -23,6 +24,38 @@ def is_photo_dir(photo_dir):
     return photo_dir_pattern.match(photo_dir) is not None
 
 
+def remove_start_end_slash(dir_path):
+    new_path = dir_path
+    if new_path.startswith('/'):
+        new_path = new_path[1:]
+    if new_path.endswith('/'):
+        new_path = new_path[:-1]
+    return new_path
+
+
+class UploadItem(object):
+
+    def __init__(self, abs_path, upload_dir, title, mimetype):
+        self.abs_path = abs_path
+        self.upload_dir = upload_dir
+        self.title = title
+        self.mimetype = mimetype
+
+    def __str__(self):
+        return "abs:{abs};dir:({dir})".format(abs=self.abs_path, dir=str(self.upload_dir))
+
+class UploadDir(object):
+
+    def __init__(self, rel_dir, parent_dir, title):
+        self.rel_dir = rel_dir
+        self.parent_dir = parent_dir
+        self.title = title
+
+    def __str__(self):
+        return "rel:{rel};parent:({parent});title:{title}".format(
+            rel=self.rel_dir, parent=str(self.parent_dir), title=self.title)
+
+
 class LocalFSManager(object):
 
     def __init__(self, root_dir):
@@ -34,22 +67,60 @@ class LocalFSManager(object):
             if os.path.isdir(
                     os.path.join(self.root_dir, o)) and is_photo_dir(o):
                 photo_dirs.add(o)
-        logging.info(
-            "Found {dirs} photo directories".format(dirs=len(photo_dirs)))
+        logging.info("Found {dirs} photo directories".format(
+            dirs=len(photo_dirs)))
         return photo_dirs
 
-    def get_photos(self, photo_dir):
-        photos = list()
-        abs_photo_dir = os.path.join(self.root_dir, photo_dir)
-        for dirpath, dnames, fnames in os.walk(abs_photo_dir):
+    def get_items(self, item_dir):
+        upload_items = list()
+        upload_dirs = dict()
+        abs_item_dir = os.path.join(self.root_dir, item_dir)
+        upload_dirs[''] = UploadDir('', None, item_dir)
+        logging.debug("Get items from: %s" % abs_item_dir)
+        for dirpath, dnames, fnames in os.walk(abs_item_dir):
+            logging.debug("Entering folder: %s" % (dirpath))
             for f in fnames:
-                abs_photo = os.path.join(dirpath, f)
-                mimetype = mime.file(abs_photo)
+                abs_item_path = os.path.join(dirpath, f)
+                mimetype = mime.file(abs_item_path)
                 # TODO: Use mime regex
                 if 'image' in mimetype or 'video' in mimetype:
-                    photos.append((abs_photo, mimetype))
-            break  # Only one level
-        return photos
+                    rel_item_dir = remove_start_end_slash(dirpath.replace(abs_item_dir, ''))
+                    if not rel_item_dir in upload_dirs:
+                        parent_dir_path, item_dir_title = os.path.split(rel_item_dir)
+                        upload_dirs[rel_item_dir] = UploadDir(rel_item_dir, upload_dirs.get(parent_dir_path), item_dir_title)
+                    upload_dir = upload_dirs[rel_item_dir]
+                    upload_item = UploadItem(
+                        abs_item_path, upload_dir, f, mimetype)
+                    upload_items.append(upload_item)
+        return upload_items, upload_dirs
+
+    def check_dir(self, photo_dir, photo_dir_files):
+        abs_photo_dir = os.path.join(self.root_dir, photo_dir)
+        logging.debug("Entering: %s" % abs_photo_dir)
+
+        subdirs = []
+        for _, sub_dirs, _ in os.walk(abs_photo_dir):
+            subdirs += sub_dirs
+            break
+
+        logging.debug("Subdirs: %s" % subdirs)
+
+        has_photo = False
+        for subdir in subdirs:
+            sub_dir_path = os.path.join(photo_dir, subdir)
+            logging.debug("Recursive: %s" % sub_dir_path)
+            has_photo = has_photo or self.check_dir(sub_dir_path)
+
+        photos = self.get_photos(abs_photo_dir)
+        logging.debug("Photos: %s" % photos)
+
+        if not has_photo and len(photos) == 0:
+            return False
+
+        if len(photos) > 0:
+            photo_dir_files[abs_photo_dir] = photos
+
+        return True
 
 
 class RemoteFSManager(object):
@@ -143,9 +214,40 @@ class UploadManager(object):
             print dir
         return selection
 
+    def upload_dir(self, local_photo_dir):
+        # Get all local photos from dir
+        local_items, local_dirs = self.local_fs.get_items(local_photo_dir)
+
+        # Create remote subdirs
+        remote_dirs = dict()
+        """
+        for item in local_items:
+            if not item.rel_dir in remote_dirs:
+                id
+                remote_dirs[]
+
+
+            # Create not existing folder
+            remote_dir = remote_fs.create_dir(
+                local_photo_dir, remote_fs.get_root_dir())
+            # Upload all photos from pending folder
+            for photo, mimetype in local_photos:
+                logging.debug("Uploading: {file}".format(file=photo))
+                remote_fs.upload_photo(photo, mimetype, remote_dir)
+
+        # TODO: Recursive upload
+        # Get dirs from dir
+        # has_photos?
+        # Get files from dir
+        # Create dir remote
+        # Upload files from dir
+        """
+        pass
+
     def process(self):
-        local_photo_dirs = local_fs.get_photo_dirs()
-        remote_photo_dirs = remote_fs.get_photo_dirs()
+        total_duration = time.time()
+        local_photo_dirs = self.local_fs.get_photo_dirs()
+        remote_photo_dirs = self.remote_fs.get_photo_dirs()
 
         # Find differences
         pending_upload_dirs = remote_fs.get_diff(
@@ -156,15 +258,14 @@ class UploadManager(object):
 
         # Create remote folders
         for local_photo_dir in selected_upload_dirs:
-            # Create not existing folder
-            remote_dir = remote_fs.create_dir(
-                local_photo_dir, remote_fs.get_root_dir())
-            # Get all local photos from dir
-            local_photos = local_fs.get_photos(local_photo_dir)
-            # Upload all photos from pending folder
-            for photo, mimetype in local_photos:
-                logging.debug("Uploading: {file}".format(file=photo))
-                remote_fs.upload_photo(photo, mimetype, remote_dir)
+            duration = time.time()
+            self.upload_dir(local_photo_dir)
+            duration = time.time() - duration
+            logging.info("Uploaded {dir} in {duration}s".format(
+                dir=local_photo_dir, duration=duration))
+        total_duration = time.time() - total_duration
+        logging.info("Processed in {duration}s".format(
+            duration=total_duration))
 
 
 if __name__ == '__main__':
